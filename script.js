4db6678bd470017b32f4082573f2d8b1b107f19d
Matheusgabriel1234 <129337670+Matheusgabriel1234@users.noreply.github.com>
2023-06-15 17:54:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+const PokemonName = document.querySelector(".pokemon__name")
+const pokemonNumber = document.querySelector(".pokemon__number")
+const Pokemonimage = document.querySelector(".pokemon__image")
+const form = document.querySelector("form")
+const inputSearch = document.querySelector(".inputSearch")
+const prev = document.querySelector(".prev")
+const next = document.querySelector(".Next")
+let searchPokemon = 1
+const fetchPokemon = async (pokemon)=>{
+ 
+    const ApiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    if(ApiResponse.status === 200){
+        const data = await ApiResponse.json()
+        return data
+    }} 
+
+
+const renderPoke = async (pokemon)=>{
+    
+    
+    const data = await fetchPokemon(pokemon)
+     if(data){ 
+    PokemonName.innerHTML = data.name
+    pokemonNumber.innerHTML = data.id
+    Pokemonimage.src = data["sprites"]["versions"]["generation-v"]["black-white"]["animated"]["front_default"]
+
+    inputSearch.value = ""
+    } else{
+        PokemonName.innerHTML = "Not found"
+        pokemonNumber.innerHTML=""
+    }
+}
+
+renderPoke(searchPokemon)
+
+form.addEventListener("submit",(evento)=>{
+
+    evento.preventDefault();
+   renderPoke(inputSearch.value)
+
+})
+
+prev.addEventListener("click",()=>{
+ searchPokemon -= 1
+ renderPoke(searchPokemon)
+})
+
+next.addEventListener("click",()=>{
+    searchPokemon++
+    renderPoke(searchPokemon)
+})
